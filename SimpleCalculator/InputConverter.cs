9c82b01a93cfb33f7c813b41363e8511251bbfe8
Ludo67 <54686907+Ludo67@users.noreply.github.com>
2020-09-14 23:52:30 -0400

    Add files via upload

---
+++ SimpleCalculator/InputConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNumeric(string argTextInput)
+        {
+            double convertedNumber;
+            if (!double.TryParse(argTextInput, out convertedNumber)) throw new ArgumentException("Expected a numeric value in numeric form. Enter '5' or '-3' , not 'five' or 'minus three' ");
+
+            return convertedNumber;
+        }
+    }
+}
